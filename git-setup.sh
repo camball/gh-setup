c9f58bfbd7027a6d98bbc76127bbe71c2b8dcab0
Cameron Ball <67163069+camball@users.noreply.github.com>
2021-10-06 04:11:08 -0400

    Update for fully-automated usage via GitHub API
    Script now uses GitHub API via PyGithub API wrapper to create the GitHub repository for you, without having to manually do it yourself at github.com. Dependencies are checked. UX updated to reflect these changes.

--- git-setup.sh
+++ git-setup.sh
@@ -2,14 +2,14 @@
 # create a new personal git repository and connect to GitHub
 # by Cameron Ball, October 2021
 
-GITHUB_USERNAME="camball"
-# GITHUB_EMAIL=$(<githubemail.txt)
+GITHUB_USERNAME="$GITHUB_USER"
+GITHUB_TOKEN="$MASTER_GITHUB_TOKEN"
 
 # uncomment following lines to set up global environment
 # git config --global init.defaultBranch main # in case git is still using "master" as default
 # git config --global color.ui true
 # git config --global user.name "$GITHUB_USERNAME"
-# git config --global user.email "$GITHUB_EMAIL"
+# git config --global user.email "your_github_email"
 
 # check cmd line args; go to directory
 if [ $# -eq 0 ]; then
@@ -21,18 +21,37 @@ if [ $# -eq 0 ]; then
             * ) echo "Please answer Y or N.";;
         esac
     done
+
+    # set 
+    while true; do
+        read -p "Use current directory title as title of GitHub repository? [y/n]: " RESPONSE
+        case $RESPONSE in
+            [Yy]* ) REPO_NAME=$(basename $PWD); break;;
+            [Nn]* ) echo ""; read -p "Enter the title of the git/GitHub repository: " REPO_NAME; break;;
+            * ) echo "Please answer Y or N.";;
+        esac
+    done
 else
     if [ ! -d "$1" ]; then
         mkdir $1 && cd $1
     else
         cd $1
     fi
+    
+    TEMP=$(basename $1)
+    while true; do
+        read -p "Use $TEMP as title of GitHub repository? [y/n]: " RESPONSE
+        case $RESPONSE in
+            [Yy]* ) REPO_NAME="$TEMP"; break;;
+            [Nn]* ) echo ""; read -p "Enter the title of the git/GitHub repository: " REPO_NAME; break;;
+            * ) echo "Please answer Y or N.";;
+        esac
+    done
 fi
 
-read -p "Enter the title of the git repository: " REPO_NAME
 echo ""
-
 git init
+echo ""
 
 echo "# $REPO_NAME" >> README.md
 
@@ -108,9 +127,40 @@ git add README.md .gitignore
 git commit -m "First commit"
 git branch -M main
 
-echo ""
-echo "Create a new GitHub repository at github.com/$GITHUB_USERNAME with title "$REPO_NAME". Press return when you have completed this step."
-read
+# create GitHub repository
+if [[ $(python3 --version) != "Python 3."* ]]; then
+    # if Python3 not installed, user can perform manual Github repository creation.
+    while true; do
+        echo "Dependency \"Python3\" not installed..."
+        read -p "Would you like to manually create the GitHub repository? [y/n]: " RESPONSE
+        case $RESPONSE in
+            [Yy]* ) 
+                echo ""
+                echo "Create a new GitHub repository at github.com/$GITHUB_USERNAME with title "$REPO_NAME". Press return when you have completed this step."
+                read; break;;
+            [Nn]* ) echo ""; echo "Exiting..."; exit;;
+            * ) echo "Please answer Y or N.";;
+        esac
+    done
+
+else # Python3 is available, so pip3 is also available
+    # make sure module PyGithub is installed
+    if [ $(pip3 list | grep -o "PyGithub") = "PyGithub" ]; then
+        :
+    else
+        echo "Dependency \`PyGithub\` not installed: attempting to install with pip3..."
+        pip3 install PyGithub
+        echo ""
+    fi
+    
+    # Create GitHub repo using PyGithub
+    python3 -c "from github import Github
+g = Github('$GITHUB_TOKEN')
+user = g.get_user()
+repo = user.create_repo('$REPO_NAME')
+quit()" # sorry for weird indentation - can't run python with bash's indentation
+    # TODO: implement error handling if repository couldn't be created for some reason
+fi
 
 git remote add origin https://github.com/$GITHUB_USERNAME/$REPO_NAME.git
 git push -u origin main
